1a010c5d224e647be19d8cd4ceb1b7fa588abe7f
mandypry <amanda_sucesso@hotmail.com>
2019-12-26 23:11:54 -0300

    Truthy_Falsy

---
+++ 004.Aula4/Truthy_Falsy.js
@@ -0,0 +1,54 @@
+/*
+Falsy: 
+undefined 0
+null  -0
+NaN - '' ou ""
+*/
+if('0'){ teste = true;} else { teste = false;}
+true
+if ('amanda'){ teste = true; } else {teste = false;}
+true
+if (undefined){ teste = true; } else {teste = false;}
+false
+if (null){ teste = true; } else {teste = false;}
+false
+if ({}){ teste = true; } else {teste = false;}
+true
+if ([]){ teste = true; } else {teste = false;}
+true
+if (NaN){ teste = true; } else {teste = false;}
+false
+if (-0){ teste = true; } else {teste = false;}
+false
+if (2){ teste = true; } else {teste = false;}
+true
+if (false){ teste = true; } else {teste = false;}
+false
+
+//descobrindo a representaçãõ Booleana
+!true
+false
+
+!1
+false
+
+1
+1
+
+!!true
+true
+
+!!1
+true
+
+!!0
+false
+
+!!""
+false
+
+!!NaN
+false
+
+!!false
+false
