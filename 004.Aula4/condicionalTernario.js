d3a3bbb4b720176fbdc1ce89584c78f9fe69fb86
mandypry <amanda_sucesso@hotmail.com>
2019-12-27 00:27:36 -0300

    Condicional e ternario

---
+++ 004.Aula4/condicionalTernario.js
@@ -0,0 +1,20 @@
+//condicao ? true : false;
+1 === 2 ? true : false;
+false
+
+var sexo = 'o';
+undefined
+
+var pessoa = { sexo: 'Masculino' };
+undefined
+
+if( pessoa.sexo === 'Feminino' ) { sexo = 'a' }
+undefined
+
+sexo
+'o'
+var sexo = pessoa.sexo === 'Feminino' ? 'a' : 'o';
+undefined
+sexo
+'o'
+
