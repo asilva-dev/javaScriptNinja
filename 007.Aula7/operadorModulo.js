ee19e89dc508c23f87423a3c5d117e596f86ca6f
mandypry <amanda_sucesso@hotmail.com>
2020-01-04 10:18:54 -0300

    Operador Modulo

---
+++ 007.Aula7/operadorModulo.js
@@ -0,0 +1,25 @@
+//Operador módulo é representado pela % e traz o resto da divisão
+3 / 3
+1
+3 % 3
+0
+5 % 2
+1
+
+var num = 0;
+while(num <= 20){
+    num % 2 === 0 ? console.log(num) : '';
+    num++;
+}
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+20
