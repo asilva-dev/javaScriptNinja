b47238580f63610e29a5d5fd47a1bec3bc85dfba
mandypry <amanda_sucesso@hotmail.com>
2020-01-05 22:25:59 -0300

    Name functions

---
+++ 008.Aula8/nameFunction.js
@@ -0,0 +1,23 @@
+var func = function() {
+    return 'hi';
+}
+undefined
+func();
+'hi'
+func
+[Function: func]
+func.name
+'func'
+func()
+'hi'
+
+var func2 = function myFunction(){}
+undefined
+func2.name
+'myFunction'
+function myFunction2(){return 'function 2';}
+undefined
+myFunction2();
+'function 2'
+myFunction2.name
+'myFunction2'
